e215412c353228a1302ddc3da456fb41616b509a
Ricky dildy <rdildy92@gmail.com>
2019-12-26 23:14:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@
 
 name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 
-def key_for_min_value(name_hash)
-  if name_hash = {}
-    return nil 
-  end 
-end 
+# def key_for_min_value(name_hash)
+#   if name_hash = {}
+#     return nil 
+#   end 
+# end 
 
 def key_for_min_value(name_hash)
   lowest_key = nil 
