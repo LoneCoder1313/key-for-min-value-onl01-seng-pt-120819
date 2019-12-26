7d77aef4d467a31b8f2b87c6c46bcb4a9421964a
Ricky dildy <rdildy92@gmail.com>
2019-12-26 23:10:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,6 @@
 def key_for_min_value(name_hash)
   if name_hash = {}
     return nil 
-  else  min_value = name_hash.first[1]
-      min_key = name_hash.first[0]
-      name_hash.each do |key, value|
-        if value < min_value
-          min_value = value
-          min_key = key
-        end
-      end
-      min_key
+  else key_for_min_value 
   end 
 end 
